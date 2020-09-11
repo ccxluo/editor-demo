d9e80d63b92f5fe2e2f29bfa656e372d5fc0f7c0
Chengxiu Luo <cxluo@Chengxius-Mac-mini.local>
2020-09-11 15:53:40 +0800

    generate markdown editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Editor from '@toast-ui/editor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-editor';
+  ngOnInit() {
+    setTimeout(() => {
+      const editor = new Editor({
+        el: document.querySelector('#editor'),
+        initialEditType: 'markdown',
+        height: '500px'
+      });
+  
+      editor.getHtml();
+    }, 0)
+  }
 }
